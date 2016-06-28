d94fb67e0868b149ba79d1fa0e54a00b7e3de71a
Isidro Sosa <isidro.sosa.p@gmail.com>
2016-06-28 15:00:13 -0500

    Fix jquery of link menu.

--- public/assets/javascripts/menu.js
+++ public/assets/javascripts/menu.js
@@ -1,3 +1,17 @@
+$(function() {
+  $('nav a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('main').scrollTop(target.offset().top);
+        $('body').removeClass('navigation-is-open');
+        return false;
+      }
+    }
+  });
+});
+
 jQuery(document).ready(function($){
 	var isLateralNavAnimating = false;
 
@@ -9,10 +23,14 @@ jQuery(document).ready(function($){
 			if($(this).parents('.csstransitions').length > 0 ) isLateralNavAnimating = true;
 
 			$('body').toggleClass('navigation-is-open');
-			$('.cd-navigation-wrapper').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+      $('.cd-navigation-wrapper').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
 				//animation is over
 				isLateralNavAnimating = false;
 			});
+      setTimeout(function() {
+        $('main').scrollTop(0);
+      }, 700);
+
 		}
 	});
 });
--- public/assets/javascripts/scrolling.js
+++ public/assets/javascripts/scrolling.js
@@ -1,12 +1,12 @@
 $(function() {
-  $('a[href*="#"]:not([href="#"])').click(function() {
+  $('main a[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
       if (target.length) {
         $('main').animate({
           scrollTop: target.offset().top
-        }, 1000);
+        }, 1500);
         return false;
       }
     }
