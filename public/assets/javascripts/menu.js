ef16fe937e14229785967bea186be8b3692a139a
Isidro Sosa <isidro.sosa.p@gmail.com>
2016-06-28 17:20:50 -0500

    Add stylesheet for menu.

--- public/assets/javascripts/menu.js
+++ public/assets/javascripts/menu.js
@@ -1,5 +1,5 @@
 $(function() {
-  $('nav a[href*="#"]:not([href="#"])').click(function() {
+  $('.cd-nav a[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
